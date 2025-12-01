b12aa84d0561f21528461332908031e38dd9fd44
Claire <ccbenson@smu.edu>
2025-11-30 22:26:39 -0600

    adding table counts, need to fix cvombined stock and sentiment table

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -2,7 +2,7 @@
 
 library(ggplot2)
 
-data <- read.csv("final_clean.csv")
+final_clean <- read.csv("final_clean.csv")
 
 #For now, plotting both raw sentiment scores and sentiment numerical scores!! 
 #This needs to be changed!! choose one! I believe it would be raw sentiment 
@@ -18,7 +18,7 @@ ggplot(final_clean, aes(x = Sentiment1, y = changepercent, color = Sentiment1)) 
 
 ggplot(final_clean, aes(x = Sentiment1_Numerical, y = changepercent, color = Sentiment1_Numerical)) +
   geom_point() +
-  labs(title = "Sentiment Scores1 vs. Actual Stock Price Changes",
+  labs(title = "Sentiment1 Scores vs. Actual Stock Price Changes",
        x = "Sentiment1",
        y = "Price Change") +
   theme_minimal()
@@ -37,3 +37,40 @@ ggplot(final_clean, aes(x = Sentiment2_Numerical, y = changepercent, color = Sen
        x = "Sentiment2",
        y = "Price Change") +
   theme_minimal()
+
+#Table counts
+all_sentiments1 <- c("positive", "negative")
+all_sentiments2 <- c("hawkish", "dovish")
+
+sentiment_table <- bind_rows(
+  final_clean %>% 
+    count(Sentiment1) %>% 
+    mutate(Type = "Sentiment1") %>% 
+    rename(Sentiment = Sentiment1) %>%
+    right_join(data.frame(Sentiment = all_sentiments1, Type = "Sentiment1"), by = c("Sentiment", "Type")) %>%
+    mutate(n = replace_na(n, 0)),
+  final_clean %>% 
+    count(Sentiment2) %>% 
+    mutate(Type = "Sentiment2") %>% 
+    rename(Sentiment = Sentiment2) %>%
+    right_join(data.frame(Sentiment = all_sentiments2, Type = "Sentiment2"), by = c("Sentiment", "Type")) %>%
+    mutate(n = replace_na(n, 0))
+)
+
+print(sentiment_table)
+
+# Group by Stock and Sentiment 
+sentiment_stock_counts <- bind_rows(
+  final_clean %>% 
+    group_by(SPchange_Numerical, Sentiment1) %>% 
+    summarise(Count = n(), .groups = 'drop') %>% 
+    rename(Sentiment = Sentiment1) %>%
+    mutate(Type = "Sentiment1"),
+  final_clean %>% 
+    group_by(SPchange_Numerical, Sentiment2) %>% 
+    summarise(Count = n(), .groups = 'drop') %>% 
+    rename(Sentiment = Sentiment2) %>%
+    mutate(Type = "Sentiment2")
+)
+
+print(sentiment_stock_counts)
