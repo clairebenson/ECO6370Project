38168735638a937edbaf8fd8f079536728d3e8d6
Dibekulu Mulu Birhan <dibekulumulubirhan@Mac.lan>
2025-11-30 23:22:44 -0600

    saving

---
+++ Code/code.R
@@ -0,0 +1,56 @@
+
+          # Cleaning #
+
+### Load packages
+library(tidyverse)
+library(lubridate)
+library(stringr)
+
+#####determine folder for data
+#setwd("~/Angie/economia/masters/computacion/ECO6370Project/Data")
+
+### Importing datasets
+st <- read_csv("Data/communications.csv")
+sk <- read_csv("Data/sap500.csv")
+
+statement <- subset(st, Type == "Statement")
+
+### Remove unwanted columns 
+cols_to_delete <- c(3, 4, 6)
+sk <- sk[ , -cols_to_delete]
+
+### Text Cleaning Function
+tokenize_and_clean <- function(text) {
+  
+  if (is.na(text)) return(NA)
+  
+  text %>%
+    tolower() %>%                                 # lowercase
+    str_replace_all("[^a-z\\s]", " ") %>%         # remove numbers & special chars
+    str_squish()                                  # remove extra spaces
+}
+
+### Apply cleaning to text column in `st`
+statement <- statement %>%
+  mutate(Text = map_chr(Text, tokenize_and_clean))
+
+### Rename columns 
+statement <- statement %>%
+  rename(
+    date = Date
+  )
+
+sk <- sk %>%
+  rename(
+    date = Date,
+    open = Open,
+    close = Close,
+    #change = change_percent
+  )
+
+
+### Merge datasets on "Date"
+final_clean <- inner_join(statement, sk, by = "date")
+
+### Save merged dataset
+write_csv(final_clean, "final_clean.csv")
