90e2c9fb90bf23e8a047ea76f26a426125068c0b
Claire <ccbenson@smu.edu>
2025-11-30 22:07:32 -0600

    scatterplots for sentiment1 and sentiment2 done , will need to choose between raw sentiment score and numerical sentiment score

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -1,8 +1,39 @@
+#Visualizations of data 
+
 library(ggplot2)
 
-ggplot(final_clean, aes(x = Sentiment, y = changepercent, color = Sentiment)) +
+data <- read.csv("final_clean.csv")
+
+#For now, plotting both raw sentiment scores and sentiment numerical scores!! 
+#This needs to be changed!! choose one! I believe it would be raw sentiment 
+
+#Scatterplot by level of Sentiment1 scores
+
+ggplot(final_clean, aes(x = Sentiment1, y = changepercent, color = Sentiment1)) +
   geom_point() +
-  labs(title = "Sentiment Scores vs. Actual Stock Price Changes",
-       x = "Sentiment",
+  labs(title = "Sentiment1 Scores vs. Actual Stock Price Changes",
+       x = "Sentiment1",
+       y = "Price Change") +
+  theme_minimal()
+
+ggplot(final_clean, aes(x = Sentiment1_Numerical, y = changepercent, color = Sentiment1_Numerical)) +
+  geom_point() +
+  labs(title = "Sentiment Scores1 vs. Actual Stock Price Changes",
+       x = "Sentiment1",
+       y = "Price Change") +
+  theme_minimal()
+
+#Scatterplot by level of Sentiment2 scores 
+ggplot(final_clean, aes(x = Sentiment2, y = changepercent, color = Sentiment2)) +
+  geom_point() +
+  labs(title = "Sentiment2 Scores vs. Actual Stock Price Changes",
+       x = "Sentiment2",
+       y = "Price Change") +
+  theme_minimal()
+
+ggplot(final_clean, aes(x = Sentiment2_Numerical, y = changepercent, color = Sentiment2_Numerical)) +
+  geom_point() +
+  labs(title = "Sentiment2 Scores vs. Actual Stock Price Changes",
+       x = "Sentiment2",
        y = "Price Change") +
   theme_minimal()
