dd3b55b8d2bca251336171b558b4582791fd4bd5
Angie Nieto <anietoriveros@smu.edu>
2025-12-01 10:51:12 -0600

    added word cloud

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -1,8 +1,68 @@
 #Visualizations of data 
 
-library(ggplot2)
+
+##### Load packages
+
+library(tidyverse)   # dplyr, tibble, ggplot2, etc.
+library(tidytext)    # tokenización y stopwords
+library(textstem)    # lematización (lemmatize_words() / lemmatize_strings())
+library(wordcloud)   # wordcloud tradicional
+library(RColorBrewer)#colors
+library(ggplot2)    
+
+### preliminary analysis
+
 
 final_clean <- read.csv("final_clean.csv")
+
+#combine all text as string
+
+all_text <- final_clean %>%
+  pull(Text) %>%             # extrae la columna Text como vector
+  na.omit() %>%              # eliminar NA si los hay
+  paste(collapse = " ")      # pegar todo con espacios
+
+### create tibble 
+
+text_tbl <- tibble(line = 1, text = all_text)
+
+# tokenize
+tokens <- text_tbl %>%
+  unnest_tokens(word, text)  # por defecto lower-case y quita puntuación básica
+
+# takeout stopwords 
+data("stop_words")           # viene con tidytext
+tokens_clean <- tokens %>%
+  anti_join(stop_words, by = "word")
+
+# eliminate numerical and 1 letter words
+tokens_clean <- tokens_clean %>%
+  filter(!str_detect(word, "^[0-9]+$")) %>%  # eliminar solo números
+  filter(nchar(word) > 1)                    # eliminar tokens de 1 carácter
+
+# Lematizar (convert "running" -> "run", etc.)
+# textstem::lemmatize_words acepta un vector de palabras
+tokens_clean <- tokens_clean %>%
+  mutate(lemma = lemmatize_words(word))
+
+# count frecuencies
+freq <- tokens_clean %>%
+  count(lemma, sort = TRUE) %>%
+  rename(word = lemma, freq = n)
+
+# Generarate
+set.seed(123)
+wordcloud(
+  words = freq$word,
+  freq = freq$freq,
+  max.words = 150,
+  random.order = FALSE,
+  scale = c(4, 0.5),
+  colors = brewer.pal(8, "Dark2")
+  
+)
+
+
 
 #For now, plotting both raw sentiment scores and sentiment numerical scores!! 
 #This needs to be changed!! choose one! I believe it would be raw sentiment 
