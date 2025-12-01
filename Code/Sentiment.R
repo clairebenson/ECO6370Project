234ad27ac3093f6f924394c0676f413b27d247d9
Claire <ccbenson@smu.edu>
2025-11-30 18:39:38 -0600

    addition of dovish/hawkosh sentiment analysis, resulting changes to final_clean files

--- Code/Sentiment.R
+++ Code/Sentiment.R
@@ -1,12 +1,11 @@
-
-          # Sentiment Scoring #
+# Sentiment Scoring 
 
 # packages #
-install.packages (c(tidyverse, tokenizers, stringer, textstem, wordcloud, Mass, class, rpart, raondomForest, gbm, caret, broom))
+install.packages (c("tidyverse", "tokenizers", "stringer", "textstem", "wordcloud", "Mass", "class", "rpart", "raondomForest", "gbm", "caret", "broom"))
 library(tidyverse)
 library(tokenizers)   # sentence tokenization
-library(stringr)      # regex & str_squish
-library(textstem)     # lemmatization (lemmatize_words)
+library(stringr)      
+library(textstem)     #lemmatize_words
 
 # +ve, -ve words #
 
@@ -47,24 +46,64 @@ negative_words <- c(
   "weaker","weakness"
 )
 
-calculate_sentiment <- function(text) {
+
+dovish_words <- c("accommodate", "accommodation", "borrowing",
+                  "devastation", "downturn", "recession",
+                  "slowdown", "stimulate", "stimulus", "unemployment")
+
+hawkish_words <- c("business", "businesses", "demand", "economic",
+             "economy", "employment", "energy", "equities",
+             "equity", "expansion", "financial", "growth",
+             "housing", "income", "indicators", "inflation",
+             "inflationary", "investment", "investments", "labor",
+             "manufacturing", "outlook", "output", "price",
+             "prices", "production", "recovery", "resource",
+             "securities", "slack", "spending", "target",
+             "toll", "wage", "wages")
+
+
+sentiment_mapping1 <- c(positive = 1, negative = 0)
+sentiment_mapping2 <- c(hawkish = 1, dovish = 0)
+
+final_clean <- read.csv("final_clean.csv")
+
+#sentiment1 is for negative/positive word classification
+calculate_sentiment1 <- function(text) {
   w <- unlist(str_split(text, "\\s+"))
-  num_pos <- sum(w %in% positive)
-  num_neg <- sum(w %in% negative)
+  num_pos <- sum(w %in% positive_words)
+  num_neg <- sum(w %in% negative_words)
   
 if (num_pos > num_neg) {"positive"} else if (num_neg > num_pos) {"negative" } else {"neutral"}
   }
 
-data_clean$Sentiment <- vapply(data_clean$Text,calculate_sentiment,FUN.VALUE = character(1))
+final_clean$Sentiment1 <- vapply(final_clean$Text,calculate_sentiment1,FUN.VALUE = character(1))
 
-# add stock move and sentiment dummies #
-data_clean <- data_clean %>% mutate(Stock = case_when(`Change%` < 0 ~ "down",`Change%` == 0 ~ "zero",TRUE ~ "up"))
-sentiment_mapping <- c(positive = 1, negative = -1, neutral = 0)
-data_clean$Sentiment_Numerical <- unname(sentiment_mapping[data_clean$Sentiment])
+# Remove neutral sentiments for Sentiment1
+final_clean <- final_clean %>% filter(Sentiment1 != "neutral")
 
-# save data #
-write.csv(final_clean, "data/final_clean.csv", row.names = FALSE)
+final_clean$Sentiment1_Numerical <- unname(sentiment_mapping1[final_clean$Sentiment1])
 
+#sentiment2 is for dovish/hawkish word classification
+calculate_sentiment2 <- function(text) {
+  w <- unlist(str_split(text, "\\s+"))
+  num_dovish <- sum(w %in% dovish_words)
+  num_hawkish <- sum(w %in% hawkish_words)
+  
+  if (num_dovish > num_hawkish) {"dovish"} else if (num_hawkish > num_dovish) {"hawkish" } else {"neutral"}
+}
 
+final_clean$Sentiment2 <- vapply(final_clean$Text,calculate_sentiment2,FUN.VALUE = character(1))
 
+# Remove neutral sentiments for Sentiment2
+final_clean <- final_clean %>% filter(Sentiment2 != "neutral")
+
+final_clean$Sentiment2_Numerical <- unname(sentiment_mapping2[final_clean$Sentiment2])
+
+# add stock move and sentiment dummies 
+final_clean$changepercent = ((final_clean$close - final_clean$open) / final_clean$open)*100
+
+final_clean <- final_clean %>% mutate(spchange = case_when(`changepercent` < 0 ~ "down",`changepercent` == 0 ~ "zero",TRUE ~ "up"))
+
+# save data 
+write.csv(final_clean, "final_clean.csv", row.names = FALSE)
 
