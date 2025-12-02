a7a604c0950783f8c644959930dc162264f980dd
Claire <ccbenson@smu.edu>
2025-12-01 22:08:55 -0600

    changes to the visualization ycode, getting rid of sentiment 2 plots and tables, adding in dj information and creating relavant plots and tables

--- Code/Visualization.R
+++ Code/Visualization.R
@@ -63,70 +63,25 @@ wordcloud(
   
 )
 
+#Scatterplot by level of Sentiment scores
 
-
-#For now, plotting both raw sentiment scores and sentiment numerical scores!! 
-#This needs to be changed!! choose one! I believe it would be raw sentiment 
-
-#Scatterplot by level of Sentiment1 scores
-
-ggplot(final_clean, aes(x = Sentiment1, y = changepercent, color = Sentiment1)) +
+ggplot(final_clean, aes(x = Sentiment1, y = sp_changepercent, color = Sentiment1)) +
   geom_point() +
-  labs(title = "Sentiment1 Scores vs. Actual Stock Price Changes",
+  labs(title = "Sentiment Scores vs. S&P Price Changes",
+       x = "Sentiment",
+       y = "Price Change") +
+  theme_minimal()
+
+ggplot(final_clean, aes(x = Sentiment1, y = dj_changepercent, color = Sentiment1)) +
+  geom_point() +
+  labs(title = "Sentiment Scores vs. DJ Price Changes",
        x = "Sentiment1",
        y = "Price Change") +
   theme_minimal()
 
-ggplot(final_clean, aes(x = Sentiment1_Numerical, y = changepercent, color = Sentiment1_Numerical)) +
-  geom_point() +
-  labs(title = "Sentiment1 Scores vs. Actual Stock Price Changes",
-       x = "Sentiment1",
-       y = "Price Change") +
-  theme_minimal()
-
-#Scatterplot by level of Sentiment2 scores 
-ggplot(final_clean, aes(x = Sentiment2, y = changepercent, color = Sentiment2)) +
-  geom_point() +
-  labs(title = "Sentiment2 Scores vs. Actual Stock Price Changes",
-       x = "Sentiment2",
-       y = "Price Change") +
-  theme_minimal()
-
-ggplot(final_clean, aes(x = Sentiment2_Numerical, y = changepercent, color = Sentiment2_Numerical)) +
-  geom_point() +
-  labs(title = "Sentiment2 Scores vs. Actual Stock Price Changes",
-       x = "Sentiment2",
-       y = "Price Change") +
-  theme_minimal()
-
-#Scatter plot of sentimet1 with spchange
-ggplot(final_clean,
-       aes(x = Sentiment1_Numerical,
-           y = spchange,
-           colour = Sentiment1)) +
-  geom_point(size = 3)
 
 #Table counts
 all_sentiments1 <- c("positive", "negative")
-all_sentiments2 <- c("hawkish", "dovish")
-
-sentiment_table <- bind_rows(
-  final_clean %>% 
-    count(Sentiment1) %>% 
-    mutate(Type = "Sentiment1") %>% 
-    rename(Sentiment = Sentiment1) %>%
-    right_join(data.frame(Sentiment = all_sentiments1, Type = "Sentiment1"), by = c("Sentiment", "Type")) %>%
-    mutate(n = replace_na(n, 0)),
-    final_clean %>% 
-    count(Sentiment2) %>% 
-    mutate(Type = "Sentiment2") %>% 
-    rename(Sentiment = Sentiment2) %>%
-    right_join(data.frame(Sentiment = all_sentiments2, Type = "Sentiment2"), by = c("Sentiment", "Type")) %>%
-    mutate(n = replace_na(n, 0))
-)
-
-print(sentiment_table)
-
 
 #Table only for sentiment1
 sentiment_table <- 
@@ -140,29 +95,16 @@ sentiment_table <-
 
 print(sentiment_table)
 
-# Group by Stock and Sentiment 
-sentiment_stock_counts <- bind_rows(
-  final_clean %>% 
-    group_by(SPchange_Numerical, Sentiment1) %>% 
-    summarise(Count = n(), .groups = 'drop') %>% 
-    rename(Sentiment = Sentiment1) %>%
-    mutate(Type = "Sentiment1"),
-  final_clean %>% 
-    group_by(SPchange_Numerical, Sentiment2) %>% 
-    summarise(Count = n(), .groups = 'drop') %>% 
-    rename(Sentiment = Sentiment2) %>%
-    mutate(Type = "Sentiment2")
-)
-
-print(sentiment_stock_counts)
-
-# Joint distribution table for Sentiment1 and spchange
-
 sentiment_table <- as.data.frame(table(final_clean$Sentiment1))
 kable(sentiment_table, caption = "Table 1. Sentiment Frequency")
 
-sentiment_stock_counts <- final_clean %>% 
+sentiment_spstock_counts <- final_clean %>% 
   count(spchange, Sentiment1, name = "Count")
 
-kable(sentiment_stock_counts, caption = "Table 2. Sentiment × SP Change")
+sentiment_djstock_counts <- final_clean %>% 
+  count(djchange, Sentiment1, name = "Count")
+
+kable(sentiment_spstock_counts, caption = "Table 2. Sentiment × SP Change")
+
+kable(sentiment_djstock_counts, caption = "Table 3. Sentiment × DJ Change")
 
